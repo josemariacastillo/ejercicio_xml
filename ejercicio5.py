7704171a38268ee429fdfec33d1ccef5b596c43f
Jose Maria Castillo <joseccotan10@gmail.com>
2016-03-02 16:20:58 +0100

    subida ejercicio5 xml prueba1

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,27 +1,20 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-#Programa que pida por teclado la latitud longitud, y muestre la URL de su ubicación del mas cercano.
+#Programa que pida por teclado la latitud longitud, y muestre la URL de su ubicación.
 
 from lxml import etree
 
 datos=etree.parse("festivales.xml")
 
-latitud=float(raw_input("Introduzca una latitud: "))
-#longitud=raw_input("Introduzca una longitud: ")
+latitud=raw_input("Introduzca una latitud: ")
+longitud=raw_input("Introduzca una longitud: ")
 
 festivales=datos.xpath("//Festival")
-resultado=[]
+
 
 for f in festivales:
-	latitudes=map(float,f.xpath("Latitud/text()"))
-	for n in latitudes:
-		print n
-		resul=(n-latitud)*1000000
-		resul = int(resul)
-		resultado.append(resul)
-
-		
-print min(resultado)/1000000
+	if latitud in f.xpath("Latitud/text()") and longitud in f.xpath("Longitud/text()"):
+		print f.xpath("URL/text()")[0]
 	
 
